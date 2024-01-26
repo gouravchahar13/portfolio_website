96a7a76387e873b804bb666bfd5c3d64943ef1cc
gouravchahar13 <1bi22cd014@bit-bangalore.edu.in>
2024-01-26 13:25:37 +0530

    updated

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
 import mimetypes
+from django.contrib import messages
+from .models import data as orignal_data
+import pandas
 
 # Create your views here.
 
@@ -10,17 +13,13 @@ def home(request):
         email=data.get('email')
         subject=data.get('subject')
         message=data.get('message')
-        print(name,message,subject,email)
+        formdata=orignal_data.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+        )
+        formdata.save()
+        messages.info(request,'Form submitted successfully,Thank you for reaching out..')
         return render(request,'index.html')
     return render(request,'index.html')
-
-# def download_file(request):
-#     # fill these variables with real values
-#     fl_path = 'C:\Users\GOURAV CHAHAR\Desktop\gourav chahar\projects\django-projects\profile_website\servingcertificate.pdf'
-#     filename = 'servingcertificate.pdf'
-
-#     fl = open(fl_path, 'r')
-#     mime_type, _ = mimetypes.guess_type(fl_path)
-#     response = HttpResponse(fl, content_type=mime_type)
-#     response['Content-Disposition'] = "attachment; filename=%s" % filename
-#     return response
